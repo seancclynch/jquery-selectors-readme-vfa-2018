2fd5ea562cadcb87911db65b923ffab3ef4fbf20
Sean Lynch <seanjcrl@gmail.com>
2018-06-30 12:42:18 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('');
+}
+Write a function ninjaBabySelector that does not accept any parameters. The function should use an ID selector to return the ninja baby image.
+
+function divSelector() {
+  return $('div.pics');
+}
+
+function firstListItem() {
+  return $('ul #pic-list ')
+}
+Write a function firstListItem that does not accept any parameters. The function should use a first-child selector to return the first list item in the ul with the ID pic-list.
